7ac21a91a4519584e7dd74eb4e4eed5346b539e0
local_xalegor <argus1672@gmail.com>
2025-11-24 14:30:06 +0500

    feat(tests): Unit test case updated

---
+++ tests/test_operations.py
@@ -0,0 +1,11 @@
+from src.math_operations import add, sub
+
+def test_add():
+	assert add(2, 3) == 5
+	assert add(-1, 1) == 0
+
+def test_sub():
+	assert sub(5, 3) == 2
+	assert sub(7, 1) == 6
+	assert sub(-1, 3) == -4
+	assert sub(10, 3) == 7
